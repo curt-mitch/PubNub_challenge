c74b43d5277871648f7c02f7100f4e0eadb8ca06
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2014-08-24 17:36:13 -0700

    Added Chosen.js, created dropdown for graphs.

--- js/dev-graphs.js
+++ js/dev-graphs.js
@@ -1,18 +1,30 @@
 // Script for importing PubNub JSON and producing data visualizations
 'use strict'
 
-$.ajax({
-  type: 'GET',
-  dataType: 'json',
-  url: '/data/sampleData.json',
-  success: handleData,
-  error: ajaxError
-})
+var channelData = {};
 
-function ajaxError() {
-  console.log('failed to fetch JSON');
-};
+$(document).ready(function() {
 
-function handleData(data) {
-  console.log('sampleData',data);
-};
+  $.ajax({
+    type: 'GET',
+    dataType: 'json',
+    url: '/data/sampleData.json',
+    success: handleData,
+    error: ajaxError
+  })
+
+  function ajaxError() {
+    console.log('failed to fetch JSON');
+  };
+
+  function handleData(data) {
+    channelData = data;
+    console.log('channelData', channelData);
+  };
+
+  // initialize Chosen multiple-select menu
+  $('#graph-select').chosen({
+    width: "25%"
+  });
+
+});
