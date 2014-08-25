fe3309588e76a34e4d1f1fbc67a4fe60ef48d12a
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2014-08-24 21:36:18 -0700

    WIP initial graph code

--- js/dev-graphs.js
+++ js/dev-graphs.js
@@ -1,5 +1,5 @@
 // Script for importing PubNub JSON and producing data visualizations
-'use strict'
+'use strict';
 
 var channelData = {};
 
@@ -11,7 +11,7 @@ $(document).ready(function() {
     url: '/data/sampleData.json',
     success: handleData,
     error: ajaxError
-  })
+  });
 
   function ajaxError() {
     console.log('failed to fetch JSON');
@@ -20,11 +20,34 @@ $(document).ready(function() {
   function handleData(data) {
     channelData = data;
     console.log('channelData', channelData);
+    parseData(channelData);
+  };
+
+  function parseData(channelData) {
+    var channelVol = channelData.channels_msg_vol;
+    var countryVol = channelData.countries_msg_vol;
+
+    // d3.select('.chart')
+    //   .selectAll('div')
+    //     .data(countryVol)
+    //   .enter().append('div')
   };
 
   // initialize Chosen multiple-select menu
   $('#graph-select').chosen({
-    width: "25%"
+    width: '25%'
   });
+
+  // declare variables for d3 graphs
+  var margin = {
+    top: 20,
+    right: 20,
+    bottom: 70,
+    left: 40
+  };
+  var width = 600 - margin.left - margin.right;
+  var height = 300 - margin.top - margin.bottom;
+
+  d3.select('.chart')
 
 });
