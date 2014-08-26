a14412d2224822e4df224bdb23fe9ea51012495f
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2014-08-25 21:27:09 -0700

    Code for channel volume chart

--- js/dev-graphs.js
+++ js/dev-graphs.js
@@ -19,19 +19,22 @@ $(document).ready(function() {
 
   function handleData(data) {
     channelData = data;
-    console.log('channelData', channelData);
     parseData(channelData);
   };
 
   function parseData(channelData) {
+    var channelVolValues = [];
     var channelVol = channelData.channels_msg_vol;
     var countryVol = channelData.countries_msg_vol;
+    for(var key in channelVol) {
+      channelVolValues.push({
+        channel: key,
+        volume: channelVol[key]
+      });
+    }
 
-    // d3.select('.chart')
-    //   .selectAll('div')
-    //     .data(countryVol)
-    //   .enter().append('div')
-  };
+    console.log('channelVol',channelVol);
+    console.log('channelVolValues', channelVolValues);
 
   // initialize Chosen multiple-select menu
   $('#graph-select').chosen({
@@ -46,8 +49,50 @@ $(document).ready(function() {
     left: 40
   };
   var width = 600 - margin.left - margin.right;
-  var height = 300 - margin.top - margin.bottom;
+  var height = 250 - margin.top - margin.bottom;
 
-  d3.select('.chart')
+  var barPadding = 1;
+  var channelObjLength = channelVolValues.length
+
+  var chart = d3.select('.chart')
+                .append('svg')
+                .attr('width', width)
+                .attr('height', height);
+
+  chart.selectAll('rect')
+      .data(channelVolValues)
+      .enter()
+      .append('rect')
+      .attr('x', function(d, i) {
+            return i * (width / channelObjLength);
+      })
+      .attr('y', function(d) {
+            return height - (d.volume * 10);
+      })
+      .attr('width', width / channelObjLength - barPadding)
+      .attr('height', function(d) {
+            return d.volume * 10;
+      })
+      .attr('fill', function(d) {
+            return "rgb(0, 0, " + (d.volume * 50) + ")";
+      });
+
+  chart.selectAll('text')
+      .data(channelVolValues)
+      .enter()
+      .append('text')
+      .text(function(d) {
+        return d.channel;
+      })
+      .attr('x', function(d, i) {
+        return i * (width / channelObjLength) + 5;
+      })
+      .attr('y', function(d) {
+        return height - (d.volume * 10) + 15;
+      })
+      .attr('font-size', '10px')
+      .attr('fill', 'white');
+
+  };
 
 });
