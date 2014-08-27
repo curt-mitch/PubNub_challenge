4a5e23f8d20844677042b8140da7d475146b3180
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2014-08-26 20:54:30 -0700

    Graph for volume by country

--- js/dev-graphs.js
+++ js/dev-graphs.js
@@ -24,6 +24,7 @@ $(document).ready(function() {
 
   function parseData(channelData) {
     var channelVolValues = [];
+    var countryVolValues = [];
     var channelVol = channelData.channels_msg_vol;
     var countryVol = channelData.countries_msg_vol;
     for(var key in channelVol) {
@@ -32,66 +33,114 @@ $(document).ready(function() {
         volume: channelVol[key]
       });
     }
+    for(var key in countryVol) {
+      countryVolValues.push({
+        country: key,
+        volume: countryVol[key]
+      });
+    }
+    console.log('countryVolValues', countryVolValues);
 
     console.log('channelVol',channelVol);
     console.log('channelVolValues', channelVolValues);
 
-  // initialize Chosen multiple-select menu
-  $('#graph-select').chosen({
-    width: '25%'
-  });
+    // initialize Chosen multiple-select menu
+    $('#graph-select').chosen({
+      width: '25%'
+    });
 
-  // declare variables for d3 graphs
-  var margin = {
-    top: 20,
-    right: 20,
-    bottom: 70,
-    left: 40
-  };
-  var width = 600 - margin.left - margin.right;
-  var height = 250 - margin.top - margin.bottom;
+    // declare variables for d3 graphs
+    var margin = {
+      top: 20,
+      right: 20,
+      bottom: 70,
+      left: 40
+    };
+    var width = 600 - margin.left - margin.right;
+    var height = 250 - margin.top - margin.bottom;
 
-  var barPadding = 1;
-  var channelObjLength = channelVolValues.length
+    var barPadding = 1;
+    var channelObjLength = channelVolValues.length
+    var countryObjLength = countryVolValues.length
 
-  var chart = d3.select('.chart')
-                .append('svg')
-                .attr('width', width)
-                .attr('height', height);
+    // chart for volume per channel
+    var channel_chart = d3.select('.channel-chart')
+                  .append('svg')
+                  .attr('width', width)
+                  .attr('height', height);
 
-  chart.selectAll('rect')
-      .data(channelVolValues)
-      .enter()
-      .append('rect')
-      .attr('x', function(d, i) {
-            return i * (width / channelObjLength);
-      })
-      .attr('y', function(d) {
-            return height - (d.volume * 10);
-      })
-      .attr('width', width / channelObjLength - barPadding)
-      .attr('height', function(d) {
-            return d.volume * 10;
-      })
-      .attr('fill', function(d) {
-            return "rgb(0, 0, " + (d.volume * 50) + ")";
-      });
+    channel_chart.selectAll('rect')
+        .data(channelVolValues)
+        .enter()
+        .append('rect')
+        .attr('x', function(d, i) {
+              return i * (width / channelObjLength);
+        })
+        .attr('y', function(d) {
+              return height - (d.volume * 10);
+        })
+        .attr('width', width / channelObjLength - barPadding)
+        .attr('height', function(d) {
+              return d.volume * 10;
+        })
+        .attr('fill', function(d) {
+              return "rgb(0, 0, " + (d.volume * 50) + ")";
+        });
 
-  chart.selectAll('text')
-      .data(channelVolValues)
-      .enter()
-      .append('text')
-      .text(function(d) {
-        return d.channel;
-      })
-      .attr('x', function(d, i) {
-        return i * (width / channelObjLength) + 5;
-      })
-      .attr('y', function(d) {
-        return height - (d.volume * 10) + 15;
-      })
-      .attr('font-size', '10px')
-      .attr('fill', 'white');
+    channel_chart.selectAll('text')
+        .data(channelVolValues)
+        .enter()
+        .append('text')
+        .text(function(d) {
+          return d.channel;
+        })
+        .attr('x', function(d, i) {
+          return i * (width / channelObjLength) + 5;
+        })
+        .attr('y', function(d) {
+          return height - (d.volume * 10) + 15;
+        })
+        .attr('font-size', '10px')
+        .attr('fill', 'white');
+
+    // chart for volume per country
+    var country_chart = d3.select('.country-chart')
+                  .append('svg')
+                  .attr('width', width)
+                  .attr('height', height);
+
+    country_chart.selectAll('rect')
+        .data(countryVolValues)
+        .enter()
+        .append('rect')
+        .attr('x', function(d, i) {
+              return i * (width / countryObjLength);
+        })
+        .attr('y', function(d) {
+              return height - (d.volume);
+        })
+        .attr('width', width / countryObjLength - barPadding)
+        .attr('height', function(d) {
+              return d.volume * 10;
+        })
+        .attr('fill', function(d) {
+              return "rgb(0, 0, " + (d.volume * 100) + ")";
+        });
+
+    country_chart.selectAll('text')
+        .data(countryVolValues)
+        .enter()
+        .append('text')
+        .text(function(d) {
+          return d.country;
+        })
+        .attr('x', function(d, i) {
+          return i * (width / countryObjLength) + 5;
+        })
+        .attr('y', function(d) {
+          return height - d.volume - 5;
+        })
+        .attr('font-size', '10px');
 
   };
 
